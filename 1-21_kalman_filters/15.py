0e2d952ac0a76d8008d9017ea8047c2e8f7e5542
Willem Bressers <dhr.bressers@gmail.com>
2021-06-25 14:07:07 +0200

    several updates

---
+++ 1-21_kalman_filters/15.py
@@ -0,0 +1,9 @@
+from math import *
+
+def update(mean1, var1, mean2, var2):
+    new_mean = (mean1 * var2 + mean2 * var1) / (var1 + var2)
+    new_var = (1 / (1 / var1 + 1 / var2))
+
+    return [new_mean, new_var]
+
+print(update(10.,8.,13., 2.))
